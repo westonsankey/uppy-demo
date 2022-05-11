340cc6219ebf76db49924dba992ccd8269b8b981
Wes Sankey <wessankey@Wess-MBP.lan>
2022-05-11 13:19:56 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useUppy } from "@uppy/react";
+import XHRUpload from "@uppy/xhr-upload";
+import Uppy from "@uppy/core";
 
 function App() {
+  console.log("rendered");
+  const uppy = useUppy(() => {
+    return new Uppy({ autoProceed: true })
+      .use(XHRUpload, {
+        endpoint: "api",
+        method: "POST",
+        formData: true,
+        fieldName: "file",
+      })
+      .on("complete", (res) => console.log(res))
+      .on("file-added", () => console.log("file added"));
+  });
+
+  const uploadFile = (file) => {
+    console.log("adding file");
+    uppy.addFile({
+      source: "file-input",
+      name: file.name,
+      type: file.type,
+      data: file,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <input
+      data-testid="file-input"
+      type="file"
+      multiple
+      accept="image/*"
+      onChange={(event) => {
+        console.log("file added");
+        const files = event.target.files ? Array.from(event.target.files) : [];
+        files.forEach(uploadFile);
+      }}
+    />
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("@uppy/react", () => ({
+  useUppy: () => {
+    const Uppy = require("@uppy/core");
+    const XHRUpload = require("@uppy/xhr-upload");
+
+    return new Uppy({ autoProceed: true })
+      .use(XHRUpload, {
+        endpoint: "api",
+        method: "POST",
+        formData: true,
+        fieldName: "file",
+      })
+      .on("complete", (res) => console.log(res))
+      .on("file-added", () => console.log("file added"));
+  },
+}));
+
+describe("App", () => {
+  it("uploads a file", async () => {
+    render(<App />);
+    const input = screen.getByTestId("file-input");
+
+    expect(input).toBeTruthy();
+
+    const file = new File([new ArrayBuffer(1)], "file.jpg");
+
+    await act(async () => {
+      userEvent.upload(input, file);
+      await new Promise((resolve) => setTimeout(resolve, 4000));
+    });
+  });
 });
